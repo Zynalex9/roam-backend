752f3a7715a51b6853ee9e1c19bc96e06297e056
Zynalex9 <alzn952@gmail.com>
2025-04-05 12:59:51 +0500

    CORS

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }))
